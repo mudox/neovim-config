37791b4404c58afc122cd59debe9152d404f100e
mudox <imudox@gmail.com>
2021-06-13 17:00:37 +0800

    new color theme `nvim-base16`

--- lua/main.lua
+++ lua/main.lua
@@ -5,4 +5,22 @@ require 'mappings'
 
 require('plug').load('plugins')
 
-vim.cmd [[color papercolor]]
+-- *
+-- * Colorscheme
+-- *
+
+-- vim.cmd [[color desert]]
+-- vim.cmd [[color monokai]]
+-- vim.cmd [[color papercolor]]
+
+local base16 = require('base16')
+base16(base16.themes['papercolor-dark'], true)
+
+local theme_names = base16.theme_names()
+local pos = 1
+function cycle_theme()
+  pos = (pos % #theme_names) + 1
+  local name = theme_names[pos]
+  print('base16: ' .. name)
+  base16(base16.themes[name], true)
+end
---
+++ lua/plugin/color/nvim-base16.lua
@@ -0,0 +1,3 @@
+install = [[
+Plug 'mudox/nvim-base16.lua'
+]]
