9fe6300e2f2ac1e0e597bb22a35a6bd452b29441
mudox <imudox@gmail.com>
2021-06-14 18:15:32 +0800

    new plugin `nvim-smoothie`

---
+++ lua/plugin/nvim-smoothie.lua
@@ -0,0 +1,8 @@
+install = [[
+Plug 'psliwka/vim-smoothie'
+]]
+
+-- function setup()
+-- Use `g.<global_var> = ...` to set global variables
+-- Use `[n|i|c|...][map|cmd|plug|nop]` to bind keys
+-- end
