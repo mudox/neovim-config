559a775f4931d48e168342af9338f0785837e1e2
mudox <imudox@gmail.com>
2021-06-14 18:15:51 +0800

    new plugin `nvim-autopairs`

---
+++ lua/plugin/nvim-autopairs.lua
@@ -0,0 +1,8 @@
+install = [[
+Plug 'windwp/nvim-autopairs'
+]]
+
+-- function setup()
+-- Use `g.<global_var> = ...` to set global variables
+-- Use `[n|i|c|...][map|cmd|plug|nop]` to bind keys
+-- end
