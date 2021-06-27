6353f650376d1f5d7e45782a4e91e0ce089f6b4f
mudox <imudox@gmail.com>
2021-06-27 11:26:03 +0800

    update path.lua

--- lua/path.lua
+++ lua/path.lua
@@ -10,36 +10,43 @@ _p.autoload = _p.root .. '/autoload'
 
 _p.lua = _p.root .. '/lua'
 _p.rock = _p.lua .. '/.rocks'
-_p.pack = _p.lua .. '/pack'
 _p.luaplugin = _p.lua .. '/plugin'
+_p.setup = _p.lua .. '/setup'
 
 -- data dir
 -- ~/.local/share/nvim
 _p.data = vim.fn.stdpath('data')
 _p.site = _p.data .. '/site'
 _p.pack = _p.site .. '/pack'
+_p.packer = _p.pack .. '/packer'
+_p.start = _p.packer .. '/start'
+_p.opt = _p.packer .. '/opt'
 
 local M = {}
 
-setmetatable(M, {
-  __index = function(_, id)
-    local r = _p[id]
-    assert(r, ('acessing invalid path id `%s`'):format(id))
-    return r
-  end,
-  __newindex = function()
-    error('`M` is a constant variable')
-  end,
-})
+setmetatable(
+    M, {
+      __index = function(_, id)
+        local r = _p[id]
+        assert(r, ('acessing invalid path id `%s`'):format(id))
+        return r
+      end,
+      __newindex = function()
+        error('`M` is a constant variable')
+      end,
+    }
+)
 
 -- Setup package paths
 
 local path_dir = M.rock .. '/share/lua/5.1'
-package.path = table.concat({
-  ([[%s/?.lua]]):format(path_dir),
-  ([[%s/?/init.lua]]):format(path_dir),
-  package.path,
-}, ';')
+package.path = table.concat(
+    {
+      ([[%s/?.lua]]):format(path_dir),
+      ([[%s/?/init.lua]]):format(path_dir),
+      package.path,
+    }, ';'
+)
 
 -- LuaFormatter off
 local cpath_dir = M.rock .. '/lib/lua/5.1'
