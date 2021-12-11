b0989ca2948c711cdf6ee319b02d3f33cf9db65e
mudox <imudox@gmail.com>
2021-12-11 08:42:18 +0800

    add mode `vscode`

--- lua/mode/vscode/post.lua
+++ lua/mode/vscode/post.lua
@@ -1,6 +1,2 @@
-local g = vim.g
--- prevent plugin loading
-g.loaded_netrwPlugin = true
-g.loaded_airline = true
-g.loaded_nvim_treesitter = true
-
+-- prevent builtin plugin loading
+vim.g.loaded_netrwPlugin = true
