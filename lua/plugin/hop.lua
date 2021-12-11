ee2730159a671a4e7bbf6460773d9adc2b81891b
mudox <imudox@gmail.com>
2021-12-11 09:27:47 +0800

    add plugin `hop`

---
+++ lua/plugin/hop.lua
@@ -0,0 +1,10 @@
+---@diagnostic disable: lowercase-global
+url = 'phaazon/hop.nvim'
+
+branch = 'v1' -- optional but strongly recommended
+
+event = 'VimEnter'
+
+function config()
+  require'hop'.setup {keys = 'etovxqpdygfblzhckisuran'}
+end
