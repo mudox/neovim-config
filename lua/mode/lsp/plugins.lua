546a873270a7578a25dba86e7f34926bd63e0288
mudox <imudox@gmail.com>
2021-12-13 13:50:04 +0800

    add mode `lsp`

---
+++ lua/mode/lsp/plugins.lua
@@ -0,0 +1,66 @@
+return {
+  -- Plugin Manager
+  'packer',
+
+  -- Lib
+  'plenary',
+
+  -- LSP
+  'lsp-config',
+  'lsp-installer',
+
+  'cmp',
+
+  'treesitter',
+
+  -- Debug
+  'dap',
+
+  -- Snippets
+  'ultisnips',
+  'snippets',
+
+  -- Editing
+  'ctrlsf',
+  'easyalign',
+  'neoformat',
+  'nerdcommenter',
+  'sandwich',
+  'switch',
+  'visualmulti',
+
+  -- UI
+  'base16',
+  'devicons',
+  'indentblankline',
+  'lualine',
+  'tmuxline',
+
+  -- Window
+  'aerial',
+  'popup',
+  'stabilize',
+  'stickybuf',
+  'telescope',
+  'zenmode',
+  -- 'twilight',
+  -- 'colorizer',
+
+  -- Text Object
+  'textobject/*',
+  'treesitter-textobjects',
+
+  -- File Type
+  'filetype/*',
+
+  -- Other
+  'startuptime',
+  'unimpaired',
+
+  -- Navigation
+  'cleverf',
+  'tmuxnavigator',
+
+  -- Lua
+  'luapad',
+}
