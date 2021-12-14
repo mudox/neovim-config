3f700dca657d232d42ae327cc1f739610fcb4470
mudox <imudox@gmail.com>
2021-12-14 22:24:12 +0800

    add `telescope` extension for `packer`

--- lua/plugin/telescope.lua
+++ lua/plugin/telescope.lua
@@ -1,10 +1,12 @@
-url = 'nvim-telescope/telescope.nvim'
+url = "nvim-telescope/telescope.nvim"
 
 requires = {
-  {'nvim-telescope/telescope-fzf-native.nvim', run = 'make'},
-  {'fannheyward/telescope-coc.nvim'},
+	{ "nvim-telescope/telescope-fzf-native.nvim", run = "make" },
+	{ "fannheyward/telescope-coc.nvim" }, -- remove it?
+	{ "nvim-telescope/telescope-ui-select.nvim" },
+	{ "nvim-telescope/telescope-packer.nvim" },
 }
 
-after = {'plenary.nvim', 'popup.nvim', 'telescope-fzf-native.nvim'}
+after = { "plenary.nvim", "popup.nvim", "telescope-fzf-native.nvim", "telescope-ui-select.nvim" }
 
-event = 'VimEnter'
+event = "VimEnter"
