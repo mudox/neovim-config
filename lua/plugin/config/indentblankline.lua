df1818ba96a8ce07ac7bfc019f333825eeb7f840
mudox <imudox@gmail.com>
2021-12-14 22:20:42 +0800

    indent-blankline: use `hightlight` style

--- lua/plugin/config/indentblankline.lua
+++ lua/plugin/config/indentblankline.lua
@@ -1,65 +1,74 @@
 -- vim: fdm=marker fmr=\ 〈,\ 〉
 
+vim.g.indent_blankline_filetype = { "lua", "rust", "swift", "c", "python" }
+
+-- Style 〈
 -- below are 4 configs exerpted from https://github.com/lukas-reineke/indent-blankline.nvim/tree/master#simple
+local style = {
+	simple = function()
+		-- vim.opt.listchars:append('space:⋅')
+		-- vim.opt.listchars:append('eol:↴')
 
-function simple()
-  -- vim.opt.listchars:append('space:⋅')
-  -- vim.opt.listchars:append('eol:↴')
+		require("indent_blankline").setup({ show_end_of_line = true })
+	end,
 
-  require('indent_blankline').setup {show_end_of_line = true}
-end
+	rainbow = function()
+		vim.cmd([[highlight IndentBlanklineIndent1 guifg=#E06C75 gui=nocombine]])
+		vim.cmd([[highlight IndentBlanklineIndent2 guifg=#E5C07B gui=nocombine]])
+		vim.cmd([[highlight IndentBlanklineIndent3 guifg=#98C379 gui=nocombine]])
+		vim.cmd([[highlight IndentBlanklineIndent4 guifg=#56B6C2 gui=nocombine]])
+		vim.cmd([[highlight IndentBlanklineIndent5 guifg=#61AFEF gui=nocombine]])
+		vim.cmd([[highlight IndentBlanklineIndent6 guifg=#C678DD gui=nocombine]])
 
-function rainbow()
-  vim.opt.termguicolors = true
-  vim.cmd [[highlight IndentBlanklineIndent1 guifg=#E06C75 gui=nocombine]]
-  vim.cmd [[highlight IndentBlanklineIndent2 guifg=#E5C07B gui=nocombine]]
-  vim.cmd [[highlight IndentBlanklineIndent3 guifg=#98C379 gui=nocombine]]
-  vim.cmd [[highlight IndentBlanklineIndent4 guifg=#56B6C2 gui=nocombine]]
-  vim.cmd [[highlight IndentBlanklineIndent5 guifg=#61AFEF gui=nocombine]]
-  vim.cmd [[highlight IndentBlanklineIndent6 guifg=#C678DD gui=nocombine]]
+		-- vim.opt.listchars:append('space:⋅')
+		-- vim.opt.listchars:append('eol:↴')
 
-  -- vim.opt.listchars:append('space:⋅')
-  -- vim.opt.listchars:append('eol:↴')
+		require("indent_blankline").setup({
+			space_char_blankline = " ",
+			char_highlight_list = {
+				"IndentBlanklineIndent1",
+				"IndentBlanklineIndent2",
+				"IndentBlanklineIndent3",
+				"IndentBlanklineIndent4",
+				"IndentBlanklineIndent5",
+				"IndentBlanklineIndent6",
+			},
+		})
+	end,
 
-  require('indent_blankline').setup {
-    space_char_blankline = ' ',
-    char_highlight_list = {
-      'IndentBlanklineIndent1',
-      'IndentBlanklineIndent2',
-      'IndentBlanklineIndent3',
-      'IndentBlanklineIndent4',
-      'IndentBlanklineIndent5',
-      'IndentBlanklineIndent6',
-    },
-  }
-end
+	fill = function()
+		vim.opt.termguicolors = true
+		vim.cmd([[highlight IndentBlanklineIndent1 guibg=#1f1f1f gui=nocombine]])
+		vim.cmd([[highlight IndentBlanklineIndent2 guibg=#1a1a1a gui=nocombine]])
 
-function background()
-  vim.opt.termguicolors = true
-  vim.cmd [[highlight IndentBlanklineIndent1 guibg=#1f1f1f gui=nocombine]]
-  vim.cmd [[highlight IndentBlanklineIndent2 guibg=#1a1a1a gui=nocombine]]
+		require("indent_blankline").setup({
+			char = "",
+			char_highlight_list = { "IndentBlanklineIndent1", "IndentBlanklineIndent2" },
+			space_char_highlight_list = {
+				"IndentBlanklineIndent1",
+				"IndentBlanklineIndent2",
+			},
+			show_trailing_blankline_indent = false,
+		})
+	end,
 
-  require('indent_blankline').setup {
-    char = '',
-    char_highlight_list = {'IndentBlanklineIndent1', 'IndentBlanklineIndent2'},
-    space_char_highlight_list = {
-      'IndentBlanklineIndent1',
-      'IndentBlanklineIndent2',
-    },
-    show_trailing_blankline_indent = false,
-  }
-end
+	highlight = function()
+		vim.cmd([[ hi IndentBlanklineChar guifg=#333333 ]])
 
-function highlight()
-  vim.opt.listchars:append('space:⋅')
-  -- vim.opt.listchars:append('eol:↴')
+		vim.opt.listchars:append("space:⋅")
+		-- vim.opt.listchars:append('eol:↴')
 
-  require('indent_blankline').setup {
-    space_char_blankline = ' ',
-    show_current_context = true,
-    show_current_context_start = true,
-  }
-end
+		require("indent_blankline").setup({
+			space_char_blankline = " ",
+			show_current_context = true,
+			show_current_context_start = false,
+		})
+	end,
+}
 
-vim.g.indent_blankline_filetype = {'lua', 'rust', 'swift', 'c', 'python'}
-rainbow()
+-- style.rainbow()
+style.highlight()
+
+-- 〉
+
+require("keymap").ncmd("yoi", "IndentBlanklineToggle")
