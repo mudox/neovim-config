ecc590ac5efe38393aa20b761474f4d6e4f52d74
mudox <imudox@gmail.com>
2021-12-16 13:48:31 +0800

    update `neovim-node-host` path

--- init.lua
+++ init.lua
@@ -1,38 +1,38 @@
 -- vim: fdm=marker fmr=\ 〈,\ 〉
---
+
 -- Penlight
-pl = require('pl.import_into')()
+pl = require("pl.import_into")()
 
 -- Mode 〈
 --
 -- if mode is not `default`, then try require `name` from 'mode.<mode_name>'
 -- otherwise require `name`
 local mode
-if vim.fn.exists('g:vscode') == 1 then
-  mode = 'vscode'
+if vim.fn.exists("g:vscode") == 1 then
+	mode = "vscode"
 else
-  mode = os.getenv('MDX_NVIM_MODE') or 'default'
+	mode = os.getenv("MDX_NVIM_MODE") or "default"
 end
 
 vim.g.mdx_nvim_mode = mode
-vim.cmd [[ lockvar g:mdx_nvim_mode ]]
+vim.cmd([[ lockvar g:mdx_nvim_mode ]])
 -- 〉
 
 -- Common paths
-stdpath = require('path')
+stdpath = require("path")
 
 -- JavaScript
-vim.g.node_host_prog = '/usr/local/bin/neovim-node-host'
+vim.g.node_host_prog = "/opt/homebrew/bin/neovim-node-host"
 
 -- Python
 vim.g.loaded_python_provider = 0
-vim.g.python3_host_prog = '/opt/homebrew/bin/python3'
+vim.g.python3_host_prog = "/opt/homebrew/bin/python3"
 
 -- Config
-require(stdpath('settings'))
-require(stdpath('mappings'))
-require('pluginmanager').load(stdpath('plugins'))
-require(stdpath('post'))
+require(stdpath("settings"))
+require(stdpath("mappings"))
+require("pluginmanager").load(stdpath("plugins"))
+require(stdpath("post"))
 
 -- packer_compiled
-vim.cmd(('source %s'):format(stdpath.packer_compiled))
+vim.cmd(("source %s"):format(stdpath.packer_compiled))
