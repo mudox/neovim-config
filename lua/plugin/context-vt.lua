ca509aa14c2aa2bbf1f7e8df4d3fb1e46f8d18ef
mudox <imudox@gmail.com>
2022-01-25 10:41:17 +0800

    add plugin `context-vt`

---
+++ lua/plugin/context-vt.lua
@@ -0,0 +1,5 @@
+url = "haringsrob/nvim_context_vt"
+
+requires = "nvim-treesitter/nvim-treesitter-textobjects"
+
+event = "BufRead"
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -16,6 +16,7 @@ return {
 	"coc",
 	"tree-sitter",
 	"dap",
+  "context-vt",
 
 	-- Editing
 	"tree-hopper",
