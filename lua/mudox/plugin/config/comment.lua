3efa8b78a2732841e38ebfe6dd20cbfbf5f20b42
mudox <imudox@gmail.com>
2022-03-05 16:26:00 +0800

    comments.nvim: use Ctrl+/ to comment quickly

--- lua/mudox/plugin/config/comment.lua
+++ lua/mudox/plugin/config/comment.lua
@@ -1,4 +1,11 @@
+local ncmd = require("mudox.keymap").ncmd
+
+ncmd("<C-_>", "gcc")
+
 local config = {
+  toggler = {
+    line = "<C-_>", -- Ctrl+/
+  },
   mappings = {
     extended = true, -- enable `g>`, `g<` mappings
   },
