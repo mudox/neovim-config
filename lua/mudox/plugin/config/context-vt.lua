e3ef0a4ddff43a9e6437d2d3292acb354e022220
mudox <imudox@gmail.com>
2022-03-08 01:07:17 +0800

    context-vt: change color, virtual text formatting

--- lua/mudox/plugin/config/context-vt.lua
+++ lua/mudox/plugin/config/context-vt.lua
@@ -1,25 +1,26 @@
 require("nvim_context_vt").setup {
   -- Override the internal highlight group name
-  -- Default is 'ContextVt'
-  highlight = "CustomContextVt",
+  highlight = "Comment",
 
   -- Disable virtual text for given filetypes
   -- Default is an empty table
   disable_ft = {},
+  disable_virtual_lines_ft = { 'python' },
 
   -- How many lines required after starting position to show virtual text
   -- Default is 1 (equals two lines total)
-  min_rows = 8,
+  min_rows = 12,
 
   -- Callback to override the generated virtual text.
   -- You can also use this to filter out node types.
-  custom_text_handler = function(node, ts_utils)
-    -- If you return `nil`, no virtual text will be displayed.
-    -- if node:type() == 'function' then
-    -- return nil
-    -- end
+  ---@diagnostic disable-next-line: unused-local
+  custom_parser = function(node, ft, opts)
+    local ts = require("nvim-treesitter.ts_utils")
+    local text = ts.get_node_text(node)[1]
 
-    -- This is the standard text
-    return "--> " .. ts_utils.get_node_text(node)[1]
+    -- trim white spaces
+    text = text:gsub("^%s*(.-)%s*$", "%1")
+
+    return " " .. text
   end,
 }
