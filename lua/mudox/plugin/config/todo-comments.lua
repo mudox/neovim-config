db8087fb5f95849cf38acb2734ec49a58693ca83
mudox <imudox@gmail.com>
2022-03-08 05:03:23 +0800

    clean code

--- lua/mudox/plugin/config/todo-comments.lua
+++ lua/mudox/plugin/config/todo-comments.lua
@@ -1,14 +1,17 @@
 -- see https://github.com/folke/todo-comments.nvim#%EF%B8%8F-configuration
+
+-- stylua: ignore start
 local opts = {
   keywords = {
-    FIX = { icon = " ", color = "error", alt = { "FIXME", "BUG", "FIXIT", "ISSUE" } },
-    TODO = { icon = " ", color = "info", alt = { "LATER" } },
-    HACK = { icon = " ", color = "warning" },
-    WARN = { icon = " ", color = "warning", alt = { "WARNING", "XXX" } },
-    PERF = { icon = " ", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
-    NOTE = { icon = " ", color = "hint", alt = { "INFO" } },
+    FIX  = { icon = " ", color = "error",   alt = { "FIXME", "BUG", "FIXIT", "ISSUE", } },
+    TODO = { icon = " ", color = "info",    alt = { "LATER", } },
+    HACK = { icon = "", color = "warning", alt = { "DANGER", } },
+    WARN = { icon = " ", color = "warning", alt = { "WARNING", } },
+    PERF = { icon = " ", color = "info",    alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE", } },
+    NOTE = { icon = " ", color = "hint",    alt = { "INFO", } },
   },
 }
+-- stylua: ignore end
 
 require("todo-comments").setup(opts)
 
