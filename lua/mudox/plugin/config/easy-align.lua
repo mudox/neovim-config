01170946230c087a7e4232534680d76fdff37e44
mudox <imudox@gmail.com>
2022-03-07 20:00:34 +0800

    lazy load `vim-easy-align`

--- lua/mudox/plugin/config/easy-align.lua
+++ lua/mudox/plugin/config/easy-align.lua
@@ -1,5 +1,4 @@
 local k = require("mudox.keymap")
 
--- WARNING: currently not work due to packer delayed loading
 k.nplug("ga", "EasyAlign")
 k.xplug("ga", "EasyAlign")
--- lua/mudox/plugin/easy-align.lua
+++ lua/mudox/plugin/easy-align.lua
@@ -1,6 +1,5 @@
 url = "junegunn/vim-easy-align"
 
-requires = "tpope/vim-repeat"
+requires = "vim-repeat"
 
--- WARNING: currently not work due to packer delayed loading
 event = "BufRead"
