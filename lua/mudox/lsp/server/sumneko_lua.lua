49c620200b9db2e3b2954daaa239b0b58676c3dc
mudox <imudox@gmail.com>
2022-03-12 17:58:37 +0800

    add vim runtime to `Lua.workspace.library`

--- lua/mudox/lsp/server/sumneko_lua.lua
+++ lua/mudox/lsp/server/sumneko_lua.lua
@@ -1,9 +1,16 @@
 return {
   settings = {
     Lua = {
-      -- https://raw.githubusercontent.com/sumneko/vscode-lua/master/setting/schema.json
+      -- Get the language server to recognize the `vim` global
       diagnostics = {
         globals = { "vim" },
+      },
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = {
+          [vim.fn.expand("$VIMRUNTIME/lua")] = true,
+          [vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
+        },
       },
     },
   },
