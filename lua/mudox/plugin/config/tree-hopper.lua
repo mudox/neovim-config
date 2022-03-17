6dd93b631b345aae69b7b5bbc2ef1afe26751ac0
mudox <imudox@gmail.com>
2022-03-18 05:34:23 +0800

    tree-hopper: improve config

--- lua/mudox/plugin/config/tree-hopper.lua
+++ lua/mudox/plugin/config/tree-hopper.lua
@@ -1,4 +1,3 @@
-vim.cmd([[
-omap     <silent> m :<C-U>lua require('tsht').nodes()<CR>
-" vnoremap <silent> m :lua require('tsht').nodes()<CR>
-]])
+local olua = require("mudox.keymap").olua
+
+olua("m", 'require("tsht").nodes()')
