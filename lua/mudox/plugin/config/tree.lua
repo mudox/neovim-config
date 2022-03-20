c7b045df08a4a47e52bdc104d566ff23fc93fad0
mudox <imudox@gmail.com>
2022-03-21 02:07:16 +0800

    add plugin `nvim-tree`

---
+++ lua/mudox/plugin/config/tree.lua
@@ -0,0 +1,50 @@
+local i = require("mudox.ui").icons
+
+vim.g.nvim_tree_show_icons = {
+  git = 0,
+  folders = 1,
+  folder_arrows = 1,
+  files = 1,
+}
+vim.g.nvim_tree_icon_padding = "  "
+vim.g.nvim_tree_special_files = {}
+
+-- stylua: ignore start
+vim.g.nvim_tree_icons = {
+  default        = " ",
+  symlink        = " ",
+  git            = {
+    unstaged     = "!",
+    staged       = "+",
+    unmerged     = "u",
+    renamed      = "r",
+    untracked    = "?",
+    deleted      = "x",
+  },
+  folder         = {
+    arrow_open   = "",
+    arrow_closed = "",
+    default      = "",
+    open         = "",
+    empty        = "",
+    empty_open   = "",
+    symlink      = "",
+    symlink_open = "",
+  },
+  lsp            = {
+    error        = i.error,
+    warning      = i.warn,
+    info         = i.info,
+    hint         = i.hint,
+  },
+}
+-- stylua: ignore end
+
+local opts = {}
+
+require("nvim-tree").setup(opts)
+
+local ncmd = require("mudox.keymap").ncmd
+
+ncmd("<M-/>t", "NvimTreeFindFileToggle")
+ncmd("<M-p>", "NvimTreeFindFileToggle")
---
+++ lua/mudox/plugin/tree.lua
@@ -0,0 +1,7 @@
+url = "kyazdani42/nvim-tree.lua"
+
+requires = "nvim-web-devicons"
+
+cmd = { "NvimTreeToggle", "NvimTreeRefresh", "NvimTreeFindFile", "NvimTreeFindFileToggle" }
+
+keys = { "<M-p>", "<M-/>t" }
