64997a2abdabaa1bed3bd28dd22972fbe64a7d67
mudox <imudox@gmail.com>
2022-03-21 23:35:17 +0800

    add mode `neorg`

---
+++ lua/mudox/mode/neorg/plugins.lua
@@ -0,0 +1,122 @@
+-- vim: fdm=marker fmr=〈,〉
+
+return {
+  "fix-cursor-hold", -- fix `updatetime`
+
+  -- Boost startup time
+  "impatient", -- it will be merged into Neovim
+  "filetype", -- replace `runtime/filetype.vim`
+
+  -- The Plugin manager
+  "packer",
+
+  -- Dev
+  "plenary",
+  "repeat",
+  "startup-time",
+
+  -- Language
+  "tree-sitter",
+
+  -- "context-vt",
+  -- "aerial",
+
+  -- "neoformat",
+
+  "trouble",
+
+  -- LSP
+  -- "lsp",
+  -- "null-ls",
+  -- "project",
+
+  -- Completion
+  "cmp",
+
+  -- Debugging
+  -- "dap",
+  -- "dap-ui",
+  -- "dap-virtual-text",
+
+  -- Editing
+  "visual-multi",
+
+  "comment",
+
+  "switch",
+
+  -- Pairing
+  "sandwich",
+  "auto-pairs",
+
+  "easy-align",
+
+  -- Search & replace
+  "spectre",
+
+  -- Snippets
+  "luasnip",
+  -- "ultisnips",
+  -- "snippets",
+
+  -- UI
+  "colorizer",
+
+  "base16",
+
+  "dev-icons",
+
+  "dressing",
+
+  "git-signs",
+
+  "lualine",
+  "tmuxline",
+
+  "indent-blank-line",
+
+  "todo-comments",
+
+  -- Window & Panes
+  "popup",
+
+  "which-key",
+  "legendary",
+
+  "notify",
+
+  "toggle-term",
+
+  "zen-mode",
+
+  -- Navigation
+  "clever-f",
+  "light-speed",
+  "tree-hopper",
+  "reach",
+
+  "nnn",
+  "tree",
+
+  "telescope",
+
+  "tmux-navigator",
+
+  -- Text object
+  "textobject/*",
+
+  -- File type
+  "filetype/*",
+
+  -- Other
+  "unimpaired",
+
+  -- Git
+  "fugitive",
+
+  -- Web
+  "emmet",
+
+  -- Notes
+  "neorg",
+}
