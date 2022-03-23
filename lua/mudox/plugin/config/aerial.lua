865f38f302bc1bc040c5a524326e74ba8c764bf9
mudox <imudox@gmail.com>
2022-03-23 09:02:25 +0800

    aerial: add more configs

--- lua/mudox/plugin/config/aerial.lua
+++ lua/mudox/plugin/config/aerial.lua
@@ -6,4 +6,18 @@ ncmd("<M-/>A", "AerialOpen")
 ncmd("]a", "AerialNext")
 ncmd("[a", "AerialPrev")
 
-require("aerial").setup {}
+require("aerial").setup {
+  highlight_on_hover = true,
+
+  min_width = 20,
+
+  post_jump_cmd = "normal! zzzv",
+
+  show_guides = true,
+  guides = {
+    mid_item = "├─",
+    last_item = "└─",
+    nested_top = "│ ",
+    whitespace = "",
+  },
+}
