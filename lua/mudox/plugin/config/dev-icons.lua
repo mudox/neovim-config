c3fc6b5b721e4300bc8dcac52d8ee9767be3ae65
mudox <imudox@gmail.com>
2022-03-26 02:41:07 +0800

    add `java`, `.gitattributes` settings to `dev-icons`

--- lua/mudox/plugin/config/dev-icons.lua
+++ lua/mudox/plugin/config/dev-icons.lua
@@ -3,29 +3,42 @@ dev_icons.setup {}
 
 local icons = dev_icons.get_icons()
 
+-- NOTE: do not put spaces in `name` field
+-- It is used for `hi` command as group name
+
 -- stylua: ignore start
-icons[".gitignore"].icon  = " "
-icons[".gitignore"].color = "#e94e31"
+icons[".gitattributes"].icon = " "
+icons[".gitignore"].icon     = " "
+-- icons[".gitignore"].color    = "#e94e31"
 
-icons["md"].icon          = " "
-icons["md"].color         = "#fff"
+icons["md"].icon             = " "
+icons["md"].color            = "#fff"
 
-icons["toml"].icon        = " "
-icons["toml"].color       = "#9c4221"
+icons["toml"].icon           = " "
+icons["toml"].color          = "#9c4221"
 
-icons["yml"].icon         = " "
-icons["yml"].color        = "#cb171d"
+icons["yml"].icon            = " "
+icons["yml"].color           = "#cb171d"
 
-icons.norg                = {
-  name                    = "Neorg",
-  icon                    = " ",
-  color                   = "#18b63a",
+icons["java"].icon           = " "
+icons["java"].color          = "#d60200"
+
+icons["class"]               = {
+  name                       = "JavaCompiledFile",
+  icon                       = " ",
+  color                      = "#666",
 }
 
-icons.bak                 = {
-  name                    = "Backup file",
-  icon                    = " ",
-  -- color                   = "#642e95",
+icons.norg                   = {
+  name                       = "Neorg",
+  icon                       = " ",
+  color                      = "#18b63a",
+}
+
+icons.bak                    = {
+  name                       = "BackupFile",
+  icon                       = " ",
+  color                      = "#666",
 }
 
 icons["zsh"].icon         = " "
@@ -33,7 +46,7 @@ icons["zsh"].icon         = " "
 
 for k, v in pairs(icons) do
   local icon = v.icon
-  icon = icon:gsub("^%s*", "") -- trim prefixing spaces
+  icon = icon:gsub("^%s+", "") -- trim prefixing spaces
   icon = icon:gsub("%s+$", "") -- trim trailing spaces
   icons[k].icon = icon
 end
