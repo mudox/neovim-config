ecca1da9a19e626e34de57c5cd0d70fdbf303e84
mudox <imudox@gmail.com>
2022-03-26 02:39:04 +0800

    separate stablize config file from spec file

--- lua/mudox/plugin/stabilize.lua
+++ lua/mudox/plugin/stabilize.lua
@@ -1,17 +1,3 @@
 url = "luukvbaal/stabilize.nvim"
 
-function config()
-  require("stabilize").setup {
-    -- stabilize window even when current cursor position will be hidden behind new window
-    force = false, -- `true` cause CocList to lose input field
-    -- set context mark to register on force event which can be jumped to with '<forcemark>
-    forcemark = nil,
-    -- do not manage windows matching these file/buftypes
-    ignore = {
-      filetype = { "help", "list", "Trouble" },
-      buftype = { "terminal", "quickfix", "loclist" },
-    },
-    -- comma-separated list of autocmds that wil trigger the plugins window restore function
-    nested = "QuickFixCmdPost,DiagnosticChanged *",
-  }
-end
+event = "VimEnter"
