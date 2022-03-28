01f2e6b34eccd5240d8d534636acc183e75da7ba
mudox <imudox@gmail.com>
2022-03-29 06:17:40 +0800

    add `spectre` config

--- lua/mudox/plugin/config/spectre.lua
+++ lua/mudox/plugin/config/spectre.lua
@@ -1,3 +1,13 @@
 local ncmd = require("mudox.keymap").ncmd
 
 ncmd("\\sp", "lua require('spectre').open()")
+
+-- stylua: ignore start
+local opts = {
+  line_sep_start = "┌──────────────────────────────────────────",
+  result_padding = "│  ",
+  line_sep       = "└──────────────────────────────────────────",
+}
+-- stylua: ignore end
+
+require("spectre").setup(opts)
