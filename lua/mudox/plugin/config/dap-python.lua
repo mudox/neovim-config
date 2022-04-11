8439b41f3103c827795c39623940055c6304b023
mudox <imudox@gmail.com>
2022-04-11 12:35:17 +0800

    dap-python: remove old keymaps

--- lua/mudox/plugin/config/dap-python.lua
+++ lua/mudox/plugin/config/dap-python.lua
@@ -2,8 +2,3 @@ local dap_python = require("dap-python")
 
 dap_python.setup("~/.local/share/nvim/debugger_adaptors/debugpy/bin/python")
 dap_python.test_runner = "pytest"
-
-local ncmd = require("mudox.keymap").ncmd
-ncmd(",dtm", "lua require('dap-python').test_method()")
-ncmd(",dtc", "lua require('dap-python').test_class()")
-ncmd(",ds", "lua require('dap-python').debug_selection()")
