6aaf236b15528e390bc7ae0d0c8aafc380265915
mudox <imudox@gmail.com>
2022-04-11 13:39:36 +0800

    indent-blank-line: improve code

--- lua/mudox/plugin/config/indent-blank-line.lua
+++ lua/mudox/plugin/config/indent-blank-line.lua
@@ -6,22 +6,24 @@ vim.g.indent_blankline_filetype = { "lua", "rust", "swift", "c", "python", "html
 -- below are 4 configs exerpted from https://github.com/lukas-reineke/indent-blankline.nvim/tree/master#simple
 local style = {
   simple = function()
-    -- vim.opt.listchars:append('space:⋅')
-    -- vim.opt.listchars:append('eol:↴')
+    vim.opt.listchars:append("space:·")
+    vim.opt.listchars:append("eol:↴")
 
     require("indent_blankline").setup { show_end_of_line = true }
   end,
 
   rainbow = function()
-    vim.cmd([[highlight IndentBlanklineIndent1 guifg=#E06C75 gui=nocombine]])
-    vim.cmd([[highlight IndentBlanklineIndent2 guifg=#E5C07B gui=nocombine]])
-    vim.cmd([[highlight IndentBlanklineIndent3 guifg=#98C379 gui=nocombine]])
-    vim.cmd([[highlight IndentBlanklineIndent4 guifg=#56B6C2 gui=nocombine]])
-    vim.cmd([[highlight IndentBlanklineIndent5 guifg=#61AFEF gui=nocombine]])
-    vim.cmd([[highlight IndentBlanklineIndent6 guifg=#C678DD gui=nocombine]])
+    vim.cmd([[
+      highlight IndentBlanklineIndent1 guifg=#E06C75 gui=nocombine
+      highlight IndentBlanklineIndent2 guifg=#E5C07B gui=nocombine
+      highlight IndentBlanklineIndent3 guifg=#98C379 gui=nocombine
+      highlight IndentBlanklineIndent4 guifg=#56B6C2 gui=nocombine
+      highlight IndentBlanklineIndent5 guifg=#61AFEF gui=nocombine
+      highlight IndentBlanklineIndent6 guifg=#C678DD gui=nocombine
+    ]])
 
-    -- vim.opt.listchars:append('space:⋅')
-    -- vim.opt.listchars:append('eol:↴')
+    vim.opt.listchars:append("space:·")
+    vim.opt.listchars:append("eol:↴")
 
     require("indent_blankline").setup {
       space_char_blankline = " ",
@@ -37,9 +39,10 @@ local style = {
   end,
 
   fill = function()
-    vim.opt.termguicolors = true
-    vim.cmd([[highlight IndentBlanklineIndent1 guibg=#1f1f1f gui=nocombine]])
-    vim.cmd([[highlight IndentBlanklineIndent2 guibg=#1a1a1a gui=nocombine]])
+    vim.cmd([[
+      highlight IndentBlanklineIndent1 guibg=#1f1f1f gui=nocombine
+      highlight IndentBlanklineIndent2 guibg=#1a1a1a gui=nocombine
+    ]])
 
     require("indent_blankline").setup {
       char = "",
@@ -53,10 +56,10 @@ local style = {
   end,
 
   highlight = function()
-    vim.cmd([[ hi IndentBlanklineChar guifg=#333333 ]])
+    vim.cmd([[highlight IndentBlanklineChar guifg=#333333]])
 
-    vim.opt.listchars:append("space:⋅")
-    -- vim.opt.listchars:append('eol:↴')
+    vim.opt.listchars:append("space:·")
+    vim.opt.listchars:append("eol:↴")
 
     require("indent_blankline").setup {
       space_char_blankline = " ",
@@ -67,8 +70,10 @@ local style = {
   end,
 }
 
--- style.rainbow()
-style.highlight()
+-- style.simple()
+-- style.fill()
+-- style.highlight()
+style.rainbow()
 
 -- 〉
 
