d2db586c121dcd0954098e7d84f21dd27288d0f0
Mudox <imudox@gmail.com>
2022-05-09 02:18:33 +0800

    todo: add token `IDEA`

--- lua/mudox/plugin/config/todo-comments.lua
+++ lua/mudox/plugin/config/todo-comments.lua
@@ -4,7 +4,7 @@
 local opts = {
   keywords = {
     FIX  = { icon = " ", color = "error",   alt = { "FIXME", "BUG", "FIXIT", "ISSUE", } },
-    TODO = { icon = " ", color = "info",    alt = { "LATER", } },
+    TODO = { icon = " ", color = "info",    alt = { "LATER", "IDEA", } },
     HACK = { icon = "", color = "warning", alt = { "DANGER", } },
     WARN = { icon = " ", color = "warning", alt = { "WARNING", } },
     PERF = { icon = " ", color = "info",    alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE", } },
