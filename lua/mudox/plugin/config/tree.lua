476bddeaa64b762c396f9c0d7775432d3d7ed048
Mudox <imudox@gmail.com>
2022-05-08 14:11:41 +0800

    update nvim-tree config

--- lua/mudox/plugin/config/tree.lua
+++ lua/mudox/plugin/config/tree.lua
@@ -1,18 +1,33 @@
+-- vim: fdm=marker fmr=〈,〉
+
 local i = require("mudox.ui").icons
 
-vim.g.nvim_tree_show_icons = {
-  git = 0,
-  folders = 1,
-  folder_arrows = 1,
-  files = 1,
-}
 vim.g.nvim_tree_icon_padding = "  "
 vim.g.nvim_tree_special_files = {}
 
+-- Icons 〈
+
 -- stylua: ignore start
+local folder = {
+  arrow_open     = i.expanded,
+  arrow_closed   = i.collapsed,
+  default        = i.folder.default,
+  open           = i.folder.open,
+  empty          = i.folder.empty,
+  empty_open     = i.folder.empty_open,
+  symlink        = i.folder.symlink,
+  symlink_open   = i.folder.symlink_open,
+}
+
+for k, icon in pairs(folder) do
+  icon = icon:gsub("^%s+", "") -- trim prefixing spaces
+  icon = icon:gsub("%s+$", "") -- trim trailing spaces
+  folder[k] = icon
+end
+
 vim.g.nvim_tree_icons = {
-  default        = " ",
-  symlink        = " ",
+  default        = "",
+  symlink        = "",
   git            = {
     unstaged     = "!",
     staged       = "+",
@@ -21,16 +36,7 @@ vim.g.nvim_tree_icons = {
     untracked    = "?",
     deleted      = "x",
   },
-  folder         = {
-    arrow_open   = i.expanded,
-    arrow_closed = i.collapsed,
-    default      = "",
-    open         = "",
-    empty        = "",
-    empty_open   = "",
-    symlink      = "",
-    symlink_open = "",
-  },
+  folder         = folder,
   lsp            = {
     error        = i.error,
     warning      = i.warn,
@@ -40,11 +46,28 @@ vim.g.nvim_tree_icons = {
 }
 -- stylua: ignore end
 
-local opts = {}
+-- 〉
+
+local opts = {
+  view = {
+    hide_root_folder = true,
+    width = 40,
+  },
+  filters = {
+    dotfiles = true,
+  },
+  renderer = {
+    indent_markers = {
+      enable = true,
+    },
+    icons = {
+      git_placement = "after",
+    },
+  },
+}
 
 require("nvim-tree").setup(opts)
 
 local ncmd = require("mudox.keymap").ncmd
 
-ncmd("<M-/>t", "NvimTreeFindFileToggle")
 ncmd("<M-p>", "NvimTreeFindFileToggle")
