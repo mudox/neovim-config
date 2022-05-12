044c3b2d135809b4907b68fa4b958d7654480083
Mudox <imudox@gmail.com>
2022-05-12 09:26:41 +0800

    fix `get_node_text` warning

--- lua/mudox/plugin/config/context-vt.lua
+++ lua/mudox/plugin/config/context-vt.lua
@@ -1,22 +1,17 @@
-require("nvim_context_vt").setup {
-  -- Override the internal highlight group name
+-- LATER: config structure has been updated
+-- See: https://github.com/haringsrob/nvim_context_vt#advanced-usage
+local opts = {
   highlight = "Comment",
 
-  -- Disable virtual text for given filetypes
-  -- Default is an empty table
   disable_ft = {},
   disable_virtual_lines_ft = { "python" },
 
-  -- How many lines required after starting position to show virtual text
-  -- Default is 1 (equals two lines total)
   min_rows = 12,
 
-  -- Callback to override the generated virtual text.
-  -- You can also use this to filter out node types.
   ---@diagnostic disable-next-line: unused-local
   custom_parser = function(node, ft, opts)
-    local ts = require("nvim-treesitter.ts_utils")
-    local text = ts.get_node_text(node)[1]
+    local utils = require('nvim_context_vt.utils')
+    local text = utils.get_node_text(node)[1]
 
     -- trim white spaces
     text = text:gsub("^%s*(.-)%s*$", "%1")
@@ -24,3 +19,5 @@ require("nvim_context_vt").setup {
     return " " .. text
   end,
 }
+
+require("nvim_context_vt").setup(opts)
