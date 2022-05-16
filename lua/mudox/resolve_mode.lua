6387a4f6ab6488da86caef6bad235fe8f8afc49e
Mudox <imudox@gmail.com>
2022-05-16 15:51:57 +0800

    fix mode resolving, detect empty string

--- lua/mudox/resolve_mode.lua
+++ lua/mudox/resolve_mode.lua
@@ -1,12 +1,24 @@
 -- if mode is not `default`, then try require `name` from 'mode.<mode_name>'
 -- otherwise require `name`
 
-local mode
-if vim.fn.exists("g:vscode") == 1 then
-  mode = "vscode"
-else
-  mode = os.getenv("MDX_NVIM_MODE") or os.getenv("DEFAULT_NVIM_MODE") or "lsp"
+local function resolve_mode()
+  local mode
+  if vim.fn.exists("g:vscode") == 1 then
+    return "vscode"
+  end
+
+  mode = vim.trim(os.getenv("MDX_NVIM_MODE") or "")
+  if #mode > 0 then
+    return mode
+  end
+
+  mode = vim.trim(os.getenv("DEFAULT_NVIM_MODE") or "")
+  if #mode > 0 then
+    return mode
+  end
+
+  return "lsp"
 end
 
-vim.g.mdx_nvim_mode = mode
+vim.g.mdx_nvim_mode = resolve_mode()
 vim.cmd([[ lockvar g:mdx_nvim_mode ]])
