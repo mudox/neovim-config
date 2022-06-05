7483eb978048af78ad6d8ec274c8e12796406bee
Mudox <imudox@gmail.com>
2022-06-06 04:51:43 +0800

    add checking before highlighting cursor

--- lua/mudox/lsp/highlight_cursor.lua
+++ lua/mudox/lsp/highlight_cursor.lua
@@ -1,3 +1,17 @@
+local function highlight_cursor()
+  local doit = false
+  for _, client in ipairs(vim.lsp.get_active_clients {}) do
+    if client.server_capabilities.documentHighlightProvider ~= nil and not client.is_stopped() then
+      doit = true
+      break
+    end
+  end
+
+  if doit then
+    vim.lsp.buf.document_highlight()
+  end
+end
+
 return function(client, bufnr)
   if not client.server_capabilities.documentHighlightProvider then
     return
@@ -10,7 +24,7 @@ return function(client, bufnr)
   vim.api.nvim_create_autocmd("CursorHold", {
     group = gid,
     buffer = bufnr,
-    callback = vim.lsp.buf.document_highlight,
+    callback = highlight_cursor,
   })
 
   vim.api.nvim_create_autocmd("CursorMoved", {
