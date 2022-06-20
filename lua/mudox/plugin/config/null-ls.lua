5fa3a51c7ca0990e4e340c7fce6d037d355acc92
Mudox <imudox@gmail.com>
2022-06-20 13:40:14 +0800

    minor change

--- lua/mudox/plugin/config/null-ls.lua
+++ lua/mudox/plugin/config/null-ls.lua
@@ -7,8 +7,9 @@ local a = null_ls.builtins.code_actions
 null_ls.setup {
   debug = false,
   sources = {
-    -- Formatting
-    f.prettier.with { extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } },
+    f.prettier.with {
+      extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" },
+    },
 
     -- Python
     f.black.with { extra_args = { "--fast" } },
