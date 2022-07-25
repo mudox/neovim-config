472644b0fdeac8c8572787c00e801549861c273b
Mudox <imudox@gmail.com>
2022-07-25 16:11:35 +0800

    disable `nvim_context_vt` for yml files

--- lua/mudox/plugin/config/context-vt.lua
+++ lua/mudox/plugin/config/context-vt.lua
@@ -4,13 +4,13 @@ local opts = {
   highlight = "Comment",
 
   disable_ft = {},
-  disable_virtual_lines_ft = { "python" },
+  disable_virtual_lines_ft = { "python", "yml" },
 
   min_rows = 12,
 
   ---@diagnostic disable-next-line: unused-local
   custom_parser = function(node, ft, opts)
-    local utils = require('nvim_context_vt.utils')
+    local utils = require("nvim_context_vt.utils")
     local text = utils.get_node_text(node)[1]
 
     -- trim white spaces
