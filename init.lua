7a2792c80bcb44fcb68922894fdba35a272cefe1
Mudox <imudox@gmail.com>
2022-09-02 14:09:43 +0800

    init: improve packer compiled file loading

--- init.lua
+++ init.lua
@@ -25,4 +25,16 @@ vim.api.nvim_create_autocmd("UIEnter", {
 })
 
 -- Packer
-vim.cmd(("source %s"):format(stdpath.packer_compiled))
+local path = stdpath.packer_compiled
+if vim.fn.filereadable(path) == 1 then
+  dofile(path)
+else
+  require("mudox.plugin.config.notify")
+  local text = "Compiled file does not exists in"
+    .. ("\n%s\n"):format(path)
+    .. "\n1. Try to compile it"
+    .. "\n2. Now restart Neovim manually"
+  require("notify")(text, "warn", { title = "Packer", timeout = 100000 })
+
+  vim.cmd("PackerCompile")
+end
