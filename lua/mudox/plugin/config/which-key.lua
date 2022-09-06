ff400177dce0a8cca3e6d4e1763d34172f2e5293
Mudox <imudox@gmail.com>
2022-09-07 01:23:08 +0800

    which-key: add `layout` settings

--- lua/mudox/plugin/config/which-key.lua
+++ lua/mudox/plugin/config/which-key.lua
@@ -11,6 +11,12 @@ local opts = {
     separator = "➠ ", -- symbol used between a key and it's label
     group = "+", -- symbol prepended to a group
   },
+  layout = {
+    -- height = { min = 4, max = 25 }, -- min and max height of the columns
+    -- width = { min = 20, max = 50 }, -- min and max width of the columns
+    spacing = 6, -- spacing between columns
+    align = "center", -- align columns left, center or right
+  },
 }
 
 require("which-key").setup(opts)
