98fd414a8aab6464318b612b7435c5e4e80a2467
Mudox <imudox@gmail.com>
2025-03-29 10:01:34 +0800

    helpview: improve heading appearance

--- lua/mudox/plugin/helpview.lua
+++ lua/mudox/plugin/helpview.lua
@@ -1,30 +1,28 @@
 -- stylua: ignore
 local opts = {
   vimdoc = {
-  keycodes      = { default = { enable = false } },
-  arguments     = { default = { enable = false } },
-  mention_links = { default = { icon   = ""    } },
-  notes         = {
-    default     = { icon   = "┃"   },
-    warning     = { icon   = "┃"   },
-    deprecated  = { icon   = "┃"   },
-  },
-  },
-  group_names   = { icon   = "󰏗 "  },
-  option_links  = { icon   = ""    },
-
-  -- headings      = {
-  --   heading_1   = { sign   = "󰎦 "  },
-  --   heading_2   = { sign   = "󰎩 "  },
-  --   heading_3   = { sign   = "󰎬 "  },
-  --   heading_4   = { sign   = "󰎮 "  },
-  -- },
+    headings         = {
+      heading_1      = { sign = " 1 ", },
+      heading_2      = { sign = " 2 ", },
+      heading_3      = { sign = " 3 ", },
+      heading_4      = { sign = " 4 ", },
+    },
+    keycodes         = { default = { enable = false } },
+    arguments        = { default = { enable = false } },
+    mention_links    = { default = { icon   = ""    } },
+    notes            = {
+      default        = { icon   = "┃"   },
+      warning        = { icon   = "┃"   },
+      deprecated     = { icon   = "┃"   },
+    },
+    optionlinks      = { icon   = ""    },
+    highlight_groups = { icon   = "󰏗 "  },
+    },
 }
 
 return {
   "OXY2DEV/helpview.nvim",
-  -- dir = "~/Git/neovim-plugins/helpview.nvim", -- HACK: `tbl_deep_extend` config
-  ft = "help",
   dependencies = "nvim-treesitter/nvim-treesitter",
+  event = "VeryLazy",
   opts = opts,
 }
