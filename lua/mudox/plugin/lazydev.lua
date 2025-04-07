674565bd935f8755fffc4ab707cb792b14677e41
Mudox <imudox@gmail.com>
2025-04-07 09:10:03 +0800

    feat: disable lazydev lspconfig integration

--- lua/mudox/plugin/lazydev.lua
+++ lua/mudox/plugin/lazydev.lua
@@ -2,6 +2,11 @@ return {
   "folke/lazydev.nvim",
   ft = "lua",
   opts = {
+    integrations = {
+      lspconfig = false,
+      cmp = false,
+      coq = false,
+    },
     library = {
       { path = "${3rd}/luv/library", words = { "vim%.uv" } },
     },
